2bea30ff216e0b0a38959556b25fa4b1b579b8c1
Arpit Bhatt <110182832+ArpitB95@users.noreply.github.com>
2022-09-03 16:32:55 +0100

    provision script for app edited

--- environment/app/provision.sh
+++ environment/app/provision.sh
@@ -16,7 +16,7 @@ sudo systemctl enable nginx
 sudo apt-get install python -y
 
 # install nodejs
-#sudo apt-get install python-software-properties -y
+sudo apt-get install python-software-properties -y
 
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
 sudo apt-get install nodejs -y
