1a1ba2e7e95c5035eef81c99fa954cc8baac8077
Arpit Bhatt <110182832+ArpitB95@users.noreply.github.com>
2022-09-03 13:52:59 +0100

    provision script for db edited

--- environment/db/provision.sh
+++ environment/db/provision.sh
@@ -1,17 +1,51 @@
 # be careful of these keys, they will go out of date
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv D68FA50FEA312927
-echo "deb https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
+#sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv D68FA50FEA312927
+#echo "deb https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
 
-sudo apt-get update -y
-sudo apt-get upgrade -y
+#sudo apt-get update -y
+#sudo apt-get upgrade -y
 
 # sudo apt-get install mongodb-org=3.2.20 -y
-sudo apt-get install -y mongodb-org=3.2.20 mongodb-org-server=3.2.20 mongodb-org-shell=3.2.20 mongodb-org-mongos=3.2.20 mongodb-org-tools=3.2.20
+#sudo apt-get install -y mongodb-org=3.2.20 mongodb-org-server=3.2.20 mongodb-org-shell=3.2.20 mongodb-org-mongos=3.2.20 mongodb-org-tools=3.2.20
 
 # remoe the default .conf and replace with our configuration
-sudo rm /etc/mongod.conf
-sudo ln -s /home/ubuntu/environment/mongod.conf /etc/mongod.conf
+#sudo rm /etc/mongod.conf
+#sudo ln -s /home/ubuntu/environment/mongod.conf /etc/mongod.conf
 
 # if mongo is is set up correctly these will be successful
-sudo systemctl restart mongod
+#sudo systemctl restart mongod
+#sudo systemctl enable mongod
+
+
+
+
+
+
+
+
+
+## My script
+
+# update & upgrade ubuntu
+sudo apt-get update
+sudo apt-get upgrade -y
+
+# retrieve key from mongodb
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv D68FA50FEA312927
+
+# Install required packages
+echo "deb https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
+
+# updates ubuntu
+sudo apt-get update
+sudo apt-get upgrade -y
+
+# Install mongodb
+sudo apt-get install -y mongodb-org=3.2.20 mongodb-org-server=3.2.20 mongodb-org-shell=3.2.20 mongodb-org-mongos=3.2.20 mongodb-org-tools=3.2.20
+
+# enabl mongodb
 sudo systemctl enable mongod
+
+# start mongodb
+sudo systemctl restart mongod
+
